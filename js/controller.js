6f99a2103210eaf5aa264bf87b0779cd36042c33
Ferdy Setiady <fd.se7en@gmail.com>
2014-03-05 03:37:00 +0700

    menu overlay lagi

--- js/controller.js
+++ js/controller.js
@@ -10,15 +10,27 @@ $("#slider").responsiveSlides({
 
 $(function() {
 	$('#menu').click(function(){ 
-		//$('#main').animate({'margin-left':'-10%'},300,function(){});                       		
+		$('#main').animate({'margin-left':'-50px'},300,function(){});  
+		$("body").addClass("no_scroll");
+		$("#overlay").addClass("tutup");                   		
 		$("#menu_area").show();
 		$('#menu_area').animate({'right':'0px'},300,function(){
 		});            
 	});
 	
 	$('#close').click(function(){
+		$("body").removeClass("no_scroll");  
+		$("#overlay").removeClass("tutup"); 
 		$('#main').animate({'margin-left':'0px'},300,function(){});
-	    $('#menu_area').animate({'right':'-300px'},300,function(){  
+	    $('#menu_area').animate({'right':'-270px'},300,function(){  
+	    $("#menu_area").hide();
+	    });
+	});
+	$('#overlay').click(function(){
+		$("body").removeClass("no_scroll");  
+		$("#overlay").removeClass("tutup"); 
+		$('#main').animate({'margin-left':'0px'},300,function(){});
+	    $('#menu_area').animate({'right':'-270px'},300,function(){  
 	    $("#menu_area").hide();
 	    });
 	});
